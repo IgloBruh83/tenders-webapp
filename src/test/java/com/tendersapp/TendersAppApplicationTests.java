6be94b6515e1e681212a1d85f632152d04f33e5b
IgloBruh <127204786+IgloBruh83@users.noreply.github.com>
2025-03-30 17:22:58 +0300

    Initial commit

---
+++ src/test/java/com/tendersapp/TendersAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tendersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TendersAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
