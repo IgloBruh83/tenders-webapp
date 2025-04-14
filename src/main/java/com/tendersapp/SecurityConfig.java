3cb234620ddabc7419a4e553d37ca6a0037071c7
IgloBruh <DmShadow83@gmail.com>
2025-04-15 00:44:22 +0300

    Added SecurityConfig

---
+++ src/main/java/com/tendersapp/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.tendersapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/register", "/register.html", "/login.html", "/css/**", "/js/**", "/images/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults()); // якщо хочеш дозволити стандартну форму логіну
+
+        return http.build();
+    }
+}
