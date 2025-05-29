515647fb4271c3d23cfd0663c53ee27d9111e5e5
IgloBruh <DmShadow83@gmail.com>
2025-05-29 21:31:02 +0300

    Minor Fix

--- src/main/java/com/tendersapp/repository/ProposalRepository.java
+++ src/main/java/com/tendersapp/repository/ProposalRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ProposalRepository extends JpaRepository<Proposal, Integer> {
-    Optional<Proposal> findByID(String title);
+    Optional<Proposal> findById(String title);
 }
